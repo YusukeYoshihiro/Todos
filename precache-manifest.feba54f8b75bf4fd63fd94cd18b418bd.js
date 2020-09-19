87cda55bb7eb8e0563e1bd16ad5eda0f8943e06d
YusukeYoshihiro <yusuke.10.25.61@gmail,com>
2020-09-19 10:50:18 -0700

    Updates

---
+++ precache-manifest.feba54f8b75bf4fd63fd94cd18b418bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0da94138da697e36e63a8ac27ebe2d0c",
+    "url": "/Todos/index.html"
+  },
+  {
+    "revision": "35ad13d79940f688615e",
+    "url": "/Todos/static/js/2.fddab1d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todos/static/js/2.fddab1d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e0e2e3f11d8aa778a3f",
+    "url": "/Todos/static/js/main.93ae718e.chunk.js"
+  },
+  {
+    "revision": "1471ff196e53b9b5dee9",
+    "url": "/Todos/static/js/runtime-main.7773c5ff.js"
+  }
+]);
