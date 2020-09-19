1dd6cf53dbe8ef39378df87dcaa5891377fdd64b
YusukeYoshihiro <yusuke.10.25.61@gmail,com>
2020-09-19 10:07:11 -0700

    todo-list

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Todos from './components/Todos';
+
+const App = () => {
+  return(
+      <Todos/>
+      // Basically, in "App.js" file, we should put tags included links of derectries which relevant to each components like "./components".
+  );
+}
+
+export default App;
